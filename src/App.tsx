dde3262347744675ae9038abfbc153023fbef433
Ezequiel Brilhante <ezequielrb7@gmail.com>
2024-02-01 12:28:08 -0300

    create header component - #f272

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme/default";
 import { GlobalStyle } from './styles/global';
+import { Transactions } from "./pages/Transactions";
 
 
 export function App() {
@@ -8,9 +9,7 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
-      <h1>
-        hello world
-      </h1>
+      <Transactions />
     </ThemeProvider>
   )
 }
