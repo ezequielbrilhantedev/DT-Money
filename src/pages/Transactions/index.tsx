d7893f18c6475489b9f7a23b6173808f44e30a48
Ezequiel Brilhante <ezequielrb7@gmail.com>
2024-02-05 15:37:17 -0300

    create search input component - class #f276

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,7 @@
 import { Summary } from '../../components/Summary';
 import { Header } from './../../components/Header/index';
+import { SearchForm } from './components/SearchForm';
+import { PriceHighlight, TransactionsContainer, TransactionsTable } from './styles';
 
 
 export function Transactions() {
@@ -7,6 +9,31 @@ export function Transactions() {
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <PriceHighlight variant='income'>R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2022</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburguer</td>
+              <td>
+                <PriceHighlight variant='outcome'>- R$ 59,00</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>10/04/2022</td>
+            </tr>
+
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
