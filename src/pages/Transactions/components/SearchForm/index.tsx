b8c355ab62c5d752ff930e47ed3d7e70a3a43dd2
Ezequiel Brilhante <ezequielrb7@gmail.com>
2024-02-08 16:07:09 -0300

    add axios e fazendo chamadas http

--- src/pages/Transactions/components/SearchForm/index.tsx
+++ src/pages/Transactions/components/SearchForm/index.tsx
@@ -3,6 +3,8 @@ import { SearchFormContainer } from "./styles";
 import { useForm } from "react-hook-form";
 import * as z from 'zod'
 import { zodResolver } from '@hookform/resolvers/zod'
+import { useContext } from "react";
+import { TransactionsContex } from "../../../../contexts/TransactionsContext";
 
 const searchFormSchema = z.object({
   query: z.string()
@@ -11,6 +13,9 @@ const searchFormSchema = z.object({
 type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+
+  const { fetchTransactions } = useContext(TransactionsContex)
+
   const {
     register,
     handleSubmit,
@@ -20,8 +25,7 @@ export function SearchForm() {
   })
 
   async function handleSearchTransactions(data: SearchFormInputs) {
-    await new Promise(resolve => setTimeout(resolve, 2000))
-    console.log(data)
+    await fetchTransactions(data.query)
   }
 
   return (
